6c16dda75cd31e3de72be7f9c5d54ab4635e7121
Ravneet Bhullar <Ravneet Bhullar@192.168.2.113>
2018-11-23 00:38:28 -0500

    intial project

---
+++ src/initarray/InitArray.java
@@ -0,0 +1,55 @@
+
+package initarray;
+
+/**
+ *
+ * @author Ravneet Bhullar
+ */
+public class InitArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] array = new int[15];
+        System.out.printf("%s%8s%n","Index","Value");
+        for (int counter = 0;counter < array.length; counter++){
+            System.out.printf("%5d%8d%n",counter, array[counter] );
+         }   
+//         int[] array = {15, 25, 34, 56, 89, 99, 23, 11,23};
+//        System.out.printf("%s%8s%n","Index","Value");
+//        for (int counter = 0;counter<array.length; counter++){
+//            System.out.printf("%5d%8d%n", counter, array[counter]);
+//        }
+        
+//      final int ARRAY_LENGTH = 10;
+//      int[] array = new int[ARRAY_LENGTH];
+//    for(int counter = 0;counter<array.length; counter++) {
+//        array[counter] = 2+2 * counter;
+//    }
+//         System.out.printf("%s%8s%n","Index","Value");
+//    for (int counter = 0;counter<array.length; counter++){
+//
+//         System.out.printf("%5d%8d%n", counter, array[counter]);
+//
+//     
+//    }
+//         int[] array = {15, 25, 34, 56, 89, 99, 23, 11,23};
+//         int total = 0;
+//         for(int counter = 0; counter<array.length; counter++){
+//             total += array[counter];
+//             
+//         }
+//         System.out.printf("Total of array elements: %d%n", total);
+//         
+
+    
+    
+    
+    
+    
+}
+    
+}
+
+
